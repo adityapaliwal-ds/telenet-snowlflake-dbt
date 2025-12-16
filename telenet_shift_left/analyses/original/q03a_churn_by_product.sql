5efc2e3bfe44e2412129823a6359e9fdbb639cfe
NicolasVerachtert-Datashift <nicolas.verachtert@datashift.eu>
2025-12-16 12:31:35 +0100

    streamz churn

--- telenet_shift_left/analyses/original/q03a_churn_by_product.sql
+++ telenet_shift_left/analyses/original/q03a_churn_by_product.sql
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------------
---Q3a : How many Streamz churn did we have in the last 3 months?
+--Q3a : How many Streamz churn did we have in month September 2025?
 ----------------------------------------------------------------------
 
 select
@@ -8,10 +8,12 @@ select
 from {{ source('product_offering_explorer', 'churn_products') }} c
 
 left join {{ source('product_offering_explorer', 'reporting_channel_hierarchy') }} rch
-on c.star_prim_rpt_chnnl_id = rch.star_rpt_channel_hierarchy_id
+    on c.star_prim_rpt_chnnl_id = rch.star_rpt_channel_hierarchy_id
+left join {{ source('product_offering_explorer', 'd_date') }} d
+    on c.star_date_id_churn = d.star_date_id
 where
     c.star_delete_time is null
-    and churn_month between '2025-09' and '2025-11' --specify the period range here
+    and d.month = '2025-09'
     and product_type = 'Content Product'
     and not (--To exclude technical migrations churns that happened in 2025
         star_date_id_churn between 20250201 and 20251231 --Migrations happened in 2025 from February to December
@@ -22,14 +24,7 @@ where
         (
             'Streamz Basic', 'Streamz Basic B2B',
             'Streamz Premium', 'Streamz Premium B2B',
-            'Streamz Premium+', 'Streamz Premium+ B2B',
-            'Play More', 'Play More B2B',
-            'Play Sports', 'Play Sports B2B',
-            'Netflix Basic', 'Netflix Basic B2B',
-            'Netflix Standard', 'Netflix Standard B2B',
-            'Netflix Premium', 'Netflix Premium B2B',
-            'Disney+ Standard', 'Disney+ Standard B2B',
-            'Disney+ Premium', 'Disney+ Premium B2B'
+            'Streamz Premium+', 'Streamz Premium+ B2B'
         ) 
 group by all
 order by 1
