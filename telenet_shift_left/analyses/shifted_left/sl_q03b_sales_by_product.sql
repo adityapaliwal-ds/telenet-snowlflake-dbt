7b5fc801776ccf6cd7d43712f107fc9c3192db14
NicolasVerachtert-Datashift <nicolas.verachtert@datashift.eu>
2025-12-16 12:36:10 +0100

    streamz sales/churn

--- telenet_shift_left/analyses/shifted_left/sl_q03b_sales_by_product.sql
+++ telenet_shift_left/analyses/shifted_left/sl_q03b_sales_by_product.sql
@@ -1,26 +1,21 @@
 ----------------------------------------------------------------------
---Q3b : How many Streamz sales did we have in in the last 3 months?
+--Q3b :  How many Streamz sales did we have in month September 2025
 ----------------------------------------------------------------------
 
 select sum(nr_sales) as total_sales
 
-from {{ ref('clean__sales_products') }}
+from {{ ref('clean__sales_products') }} s
+left join {{ ref('d_date') }} d
+    on s.star_date_id = d.star_date_id
 
 where
-    sales_month between '2025-09' and '2025-11' --specify the period range here
+    d.month = '2025-09'
     and product_type = 'Content Product'
     and cleaned_entertainment_product in --specify the products here (using cleaned names without B2B suffix)
         (
             'Streamz Basic', 'Streamz Basic B2B',
             'Streamz Premium', 'Streamz Premium B2B',
-            'Streamz Premium+', 'Streamz Premium+ B2B',
-            'Play More', 'Play More B2B',
-            'Play Sports', 'Play Sports B2B',
-            'Netflix Basic', 'Netflix Basic B2B',
-            'Netflix Standard', 'Netflix Standard B2B',
-            'Netflix Premium', 'Netflix Premium B2B',
-            'Disney+ Standard', 'Disney+ Standard B2B',
-            'Disney+ Premium', 'Disney+ Premium B2B'
+            'Streamz Premium+', 'Streamz Premium+ B2B'
         ) 
 group by all
 order by 1
